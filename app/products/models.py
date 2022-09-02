febb951b66c57c02e85a940d897416217c643b1d
Andrej Ceraj <andrej.ceraj@gmail.com>
2022-09-02 14:08:51 +0200

    added Product and ProductRating models

--- app/products/models.py
+++ app/products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=False, unique=True)
+    price = models.DecimalField(null=False, decimal_places=2, max_digits=12)
+    rating = models.FloatField(null=False, default=0)
+    updated_at = models.DateTimeField(null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductRating(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    value = models.PositiveSmallIntegerField(null=False)
+
+    def __str__(self):
+        return "{} rated {} with {} stars".format(self.user, self.product, self.value)
